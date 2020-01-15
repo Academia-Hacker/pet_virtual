e5e91d936305ca56aa92d6baf1850ceee9217bc7
everttonbs <ebs@ic.ufal.br>
2020-01-15 15:29:30 -0300

    new attribute

--- pet.py
+++ pet.py
@@ -1,27 +1,43 @@
 
 class Pet_Virtual:
-    __slots__ = '_name', '_happy', '_hungry'
-
-
+    __slots__ = '_name', '_age', '_happy', '_weight', '_hungry', '_sed', '_sleep', '_power'
 
     def __init__(self, name):
         self._name = name
-        self._happy = 50
-        self._hungry = 50
+        self._age = 0
+        self._happy = 50 #max 100
+        self._weight = 50 #max 100
+        self._hungry = False
+        self._sleep = False
+        self._sed = False
+        self._power = 50
     
-    def eat(self):
-        pass
+    def feed(self):
+        self._weight += 10
 
-    def drink(self):
+        if(self._weight > 80):  self._hungry = False
+        else:   self._hungry = True
+
+
+    def give_water(self):
         pass
 
     def take_a_shower(self):
         pass
 
     def play(self):
+        self._power -= 1
+        if(self._power < 10):
+            self._sed = True
+
+    def put_to_sleep(self):
         pass
 
-    def sleep(self):
+    def vaccinate(self):
         pass
+
+    def teach(self):
+        pass
+
 
 
